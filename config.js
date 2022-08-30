346dac93b50453e1a4da25e0bcc9c34aa9b3fe6d
lilpepbotz <109938232+lilpepbotz@users.noreply.github.com>
2022-08-30 20:03:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,6 +78,9 @@ global.anu = 'https://telegra.ph/file/24ef31ec559b99b3009be.jpg'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
+global.vn = './sound/menu.mp3'
+
+
 global.rpg = {
   emoticon(string) {
     string = string.toLowerCase()
